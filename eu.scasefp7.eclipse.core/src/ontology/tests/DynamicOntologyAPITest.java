a4d0be89175421c94b7373b539d3717eeb3a4a29
Marin Orlic <marin.orlic@ericsson.com>
2015-06-24 12:05:20 +0200

    Refactor to plugins

---
+++ eu.scasefp7.eclipse.core/src/ontology/tests/DynamicOntologyAPITest.java
@@ -0,0 +1,71 @@
+package ontology.tests;
+
+import ontology.DynamicOntologyAPI;
+
+/**
+ * An example instantiation of the dynamic ontology.
+ * 
+ * @author themis
+ */
+public class DynamicOntologyAPITest {
+
+	/**
+	 * Instantiates the ontology.
+	 * 
+	 * @param args unused parameter.
+	 */
+	public static void main(String[] args) {
+
+		// Create a new file for the dynamic ontology and instantiate it
+		DynamicOntologyAPI ontology = new DynamicOntologyAPI("Restmarks", true);
+
+		// Add a new diagram
+		ontology.addActivityDiagram("Add_Bookmark");
+
+		// Add a start node, an end node and any preconditions
+		ontology.addInitialActivity("StartNode");
+		ontology.connectActivityDiagramToElement("Add_Bookmark", "StartNode");
+		ontology.addPreconditionToDiagram("Add_Bookmark", "User_must_be_logged_in");
+		ontology.addFinalActivity("EndNode");
+		ontology.connectActivityDiagramToElement("Add_Bookmark", "EndNode");
+
+		// Add an individual for an activity and its properties
+		ontology.addActivity("Create_bookmark");
+		ontology.connectActivityDiagramToElement("Add_Bookmark", "Create_bookmark");
+		ontology.addActionToActivity("Create_bookmark", "create");
+		ontology.addObjectToActivity("Create_bookmark", "bookmark");
+		ontology.addActivityTypeToActivity("Create_bookmark", "create");
+		ontology.addPropertyToActivity("Create_bookmark", "Bookmark_URL");
+		ontology.addPropertyToActivity("Create_bookmark", "Bookmark_Name");
+
+		// Add an individual for another activity
+		ontology.addActivity("Add_tag");
+		ontology.connectActivityDiagramToElement("Add_Bookmark", "Add_tag");
+		ontology.addActionToActivity("add", "Add_tag");
+		ontology.addObjectToActivity("tag", "Add_tag");
+		ontology.addActivityTypeToActivity("create", "Add_tag");
+		ontology.addPropertyToActivity("Add_tag", "Tag_text");
+
+		// Add individuals for the first transition
+		ontology.addTransition("StartNode", "Create_bookmark");
+		ontology.connectActivityDiagramToTransition("Add_Bookmark", "StartNode", "Create_bookmark");
+
+		// Add individuals for another transition (a possible path)
+		ontology.addTransition("Create_bookmark", "Add_tag");
+		ontology.connectActivityDiagramToTransition("Add_Bookmark", "Create_bookmark", "Add_tag");
+		ontology.addConditionToTransition("User_wants_to_add_tag__PATH__Yes", "Create_bookmark", "Add_tag");
+
+		// Add individuals for another transition (another possible path)
+		ontology.addTransition("Create_bookmark", "EndNode");
+		ontology.connectActivityDiagramToTransition("Add_Bookmark", "Create_bookmark", "EndNode");
+		ontology.addConditionToTransition("User_wants_to_add_tag__PATH__No", "Create_bookmark", "EndNode");
+
+		// Add individuals for the final transition
+		ontology.addTransition("Add_tag", "EndNode");
+		ontology.connectActivityDiagramToTransition("Add_Bookmark", "Add_tag", "EndNode");
+
+		// Close and save the ontology
+		ontology.close();
+	}
+
+}
