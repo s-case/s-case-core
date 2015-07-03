11b8fb638a030c97ca198a8b024f76480cb8c15a
Marin Orlic <marin.orlic@ericsson.com>
2015-07-03 15:09:53 +0300

    Add UI constants reference interface

---
+++ eu.scasefp7.eclipse.core.ui/src/eu/scasefp7/eclipse/core/ui/IScaseUiConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2015 S-CASE Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.scasefp7.eclipse.core.ui;
+
+/**
+ * @author emaorli
+ *
+ */
+public interface IScaseUiConstants {
+	public static final String CATEGORY = "eu.scasefp7.eclipse.category"; //$NON-NLS-1$
+	public static final String PERSPECTIVE = "eu.scasefp7.eclipse.core.ui.ScasePerspective"; //$NON-NLS-1$
+	public static final String DASHBOARD_VIEW = "eu.scasefp7.eclipse.core.ui.views.Dashboard"; //$NON-NLS-1$
+	
+	/* These are external, included here for convenience */
+	public static final String UML_RECOGNIZER_WIZARDID = "eu.scasefp7.eclipse.umlrec.ui.wizard.MyWizard"; //$NON-NLS-1$
+	
+}
