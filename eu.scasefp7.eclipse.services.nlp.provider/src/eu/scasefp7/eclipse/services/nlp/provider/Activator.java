4ec64732ad26f2c3b04c7a157dcd44fee274fc16
Marin Orlic <marin.orlic@ericsson.com>
2015-11-09 20:09:03 +0100

    Add prefs change listener

--- eu.scasefp7.eclipse.services.nlp.provider/src/eu/scasefp7/eclipse/services/nlp/provider/Activator.java
+++ eu.scasefp7.eclipse.services.nlp.provider/src/eu/scasefp7/eclipse/services/nlp/provider/Activator.java
@@ -3,11 +3,16 @@ package eu.scasefp7.eclipse.services.nlp.provider;
 
 //import org.eclipse.ecf.osgi.services.remoteserviceadmin.RemoteServiceAdmin;
 //import org.eclipse.ecf.osgi.services.remoteserviceadmin.RemoteServiceAdmin.ExportRegistration;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.services.IServiceLocator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
 import eu.scasefp7.eclipse.services.nlp.INLPService;
+import eu.scasefp7.eclipse.services.nlp.provider.config.NLPServiceConfigurator;
 
 /**
  * @author emaorli
@@ -16,6 +21,7 @@ import eu.scasefp7.eclipse.services.nlp.INLPService;
 public class Activator implements BundleActivator {
 
     private ServiceRegistration<INLPService> nlpRegistration = null;
+    private IPreferenceChangeListener listener = null;
 //    private ServiceRegistration<ContainerTypeDescription> containerRegistration = null;
     
 	@Override
@@ -29,6 +35,14 @@ public class Activator implements BundleActivator {
 	    // this.nlpRegistration = bundleContext.registerService(INLPService.class.getName(), new INLPService(), properties)
 //	    RemoteServiceAdmin rsa = new RemoteServiceAdmin(bundleContext.getBundle());
 //	    List<ExportRegistration> srs = rsa.getExportedRegistrations();
+	
+	    
+	    IServiceLocator serviceLocator = PlatformUI.getWorkbench();
+	    IPreferencesService prefsService = (IPreferencesService) serviceLocator.getService(IPreferencesService.class);
+        
+	    this.listener = new NLPServiceConfigurator();
+	    prefsService.getRootNode().addPreferenceChangeListener(this.listener);
+	    
 	}
 	
 	
@@ -37,6 +51,14 @@ public class Activator implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 //	    this.namespaceRegistration.unregister();
 //        this.containerRegistration.unregister();
+	    
+	    if(this.listener != null) {
+	        IServiceLocator serviceLocator = PlatformUI.getWorkbench();
+	        IPreferencesService prefsService = (IPreferencesService) serviceLocator.getService(IPreferencesService.class);
+	        prefsService.getRootNode().removePreferenceChangeListener(this.listener);
+	        this.listener = null;
+	    }
+	          
 	}
         
 	
