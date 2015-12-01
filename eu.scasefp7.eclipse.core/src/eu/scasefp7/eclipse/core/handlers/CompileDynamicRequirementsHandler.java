571b5614bd543cfe0e0df78baead414827618c16
thdiaman <themisdiamantopoulos@hotmail.com>
2015-12-01 15:04:51 +0200

    Added commands for instantiating the ontologies

---
+++ eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/handlers/CompileDynamicRequirementsHandler.java
@@ -0,0 +1,26 @@
+package eu.scasefp7.eclipse.core.handlers;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * A command handler for exporting all Storyboards and Activity Diagram XMI files to the dynamic ontology.
+ * 
+ * @author themis
+ */
+public class CompileDynamicRequirementsHandler extends CommandExecutorHandler {
+
+	/**
+	 * This function is called when the user selects the menu item. It populates the dynamic ontology.
+	 * 
+	 * @param event the event containing the information about which file was selected.
+	 * @return the result of the execution which must be {@code null}.
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		executeCommand("eu.scasefp7.eclipse.storyboards.commands.exportAllToOntology");
+		executeCommand("eu.scasefp7.eclipse.umlrec.commands.exportActivityDiagramsToOntology");
+		return null;
+	}
+
+}
