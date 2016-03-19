4d5bd42e098404a0d9c25aed44359c5d58ba9c84
thdiaman <themisdiamantopoulos@hotmail.com>
2016-03-19 14:22:23 +0200

    Update project-aware handler to support also selecting folders

--- eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/handlers/ProjectAwareHandler.java
+++ eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/handlers/ProjectAwareHandler.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -37,18 +37,18 @@ public abstract class ProjectAwareHandler extends AbstractHandler {
 	}
 
 	/**
-	 * Returns the project that the selected file(s) belong to.
+	 * Returns the project that the selected resource(s) belong to.
 	 * 
-	 * @param selectionList the selected file(s).
-	 * @return the project that the selected file(s) belong to.
+	 * @param selectionList the selected resource(s).
+	 * @return the project that the selected resource(s) belong to.
 	 */
 	protected IProject getProjectOfSelectionList(List<Object> selectionList) {
 		IProject project = null;
 		for (Object object : selectionList) {
-			IFile file = (IFile) Platform.getAdapterManager().getAdapter(object, IFile.class);
+			IResource resource = (IResource) Platform.getAdapterManager().getAdapter(object, IResource.class);
 			IProject theproject = null;
-			if (file != null) {
-				theproject = file.getProject();
+			if (resource != null) {
+				theproject = resource.getProject();
 			} else {
 				theproject = (IProject) Platform.getAdapterManager().getAdapter(object, IProject.class);
 			}
