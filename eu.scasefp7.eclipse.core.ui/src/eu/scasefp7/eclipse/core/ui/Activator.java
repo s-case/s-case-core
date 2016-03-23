b3866c99c43921b42278a6dbf439f8c859aac3bb
Marin Orlic <marin.orlic@ericsson.com>
2016-03-23 21:58:43 +0100

    Reset activator

--- eu.scasefp7.eclipse.core.ui/src/eu/scasefp7/eclipse/core/ui/Activator.java
+++ eu.scasefp7.eclipse.core.ui/src/eu/scasefp7/eclipse/core/ui/Activator.java
@@ -26,115 +26,115 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin implements DebugOptionsListener {
 
-	/** The plug-in ID. */
-	public static final String PLUGIN_ID = "eu.scasefp7.eclipse.core.ui"; //$NON-NLS-1$
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "eu.scasefp7.eclipse.core.ui"; //$NON-NLS-1$
 
-	/** The starting time of the current session for this plugin. */
-	private static String STARTING_TIME;
+    /** The starting time of the current session for this plugin. */
+    private static String STARTING_TIME;
 
-	/** The current error ID for this session for this plugin. */
-	private static int errorID;
+    /** The current error ID for this session for this plugin. */
+    private static int errorID;
 
-	/** The shared instance. */
-	private static Activator plugin;
+    /** The shared instance. */
+    private static Activator plugin;
 
-	/** The shared images. */
-	private static SharedImages images = null;
+    /** The shared images. */
+    private static SharedImages images = null;
 
-	/** Cached debug tracing flag. */
-	public static boolean DEBUG = false;
+    /** Cached debug tracing flag. */
+    public static boolean DEBUG = false;
 
-	/** Cached debug trace output. */
-	public static DebugTrace TRACE = null;
+    /** Cached debug trace output. */
+    public static DebugTrace TRACE = null;
 
-	/** A resource change listener for project folders. */
-	IResourceChangeListener resourceChangeListener;
+    /** A resource change listener for project folders. */
+    private IResourceChangeListener resourceChangeListener;
 
-	/**
-	 * The constructor.
-	 */
-	public Activator() {
-	}
+    /**
+     * The constructor.
+     */
+    public Activator() {
+    }
 
-	/**
-	 * Called when plugin starts. Marks the start time for logging.
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-		STARTING_TIME = Activator.oDateFormatter.format(new Date());
-		errorID = 0;
+    /**
+     * Called when plugin starts. Marks the start time for logging.
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+        STARTING_TIME = Activator.oDateFormatter.format(new Date());
+        errorID = 0;
 
-		// Register for debug trace
-		Dictionary<String, String> props = new Hashtable<String, String>(4);
-		props.put(DebugOptions.LISTENER_SYMBOLICNAME, PLUGIN_ID);
-		context.registerService(DebugOptionsListener.class.getName(), this, props);
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		resourceChangeListener = new IResourceChangeListener() {
-			public void resourceChanged(IResourceChangeEvent event) {
-				IResourceDelta delta = event.getDelta();
-				if (delta != null) {
-					BuildDeltaVisitor deltaVisitor = new BuildDeltaVisitor();
-					try {
-						delta.accept(deltaVisitor);
-					} catch (CoreException e) {
-						Activator.log("Error when handling rename or delete of project and/or project folders", e);
-					}
-				}
-			}
-		};
-		workspace.addResourceChangeListener(resourceChangeListener);
-	}
+        // Register for debug trace
+        Dictionary<String, String> props = new Hashtable<String, String>(4);
+        props.put(DebugOptions.LISTENER_SYMBOLICNAME, PLUGIN_ID);
+        context.registerService(DebugOptionsListener.class.getName(), this, props);
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        resourceChangeListener = new IResourceChangeListener() {
+            public void resourceChanged(IResourceChangeEvent event) {
+                IResourceDelta delta = event.getDelta();
+                if (delta != null) {
+                    BuildDeltaVisitor deltaVisitor = new BuildDeltaVisitor();
+                    try {
+                        delta.accept(deltaVisitor);
+                    } catch (CoreException e) {
+                        Activator.log("Error when handling rename or delete of project and/or project folders", e);
+                    }
+                }
+            }
+        };
+        workspace.addResourceChangeListener(resourceChangeListener);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		workspace.removeResourceChangeListener(resourceChangeListener);
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        IWorkspace workspace = ResourcesPlugin.getWorkspace();
+        workspace.removeResourceChangeListener(resourceChangeListener);
+        super.stop(context);
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance
+     *
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * Returns the shared image registry
-	 * 
-	 * @return the images
-	 */
-	public synchronized static SharedImages getImages() {
-	    if(images == null) {
-	        images = new SharedImages();
-	    }
-	    
-	    return images;
-	}
+    /**
+     * Returns the shared image registry
+     * 
+     * @return the images
+     */
+    public static SharedImages getImages() {
+        if (images == null) {
+            images = new SharedImages();
+        }
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
+        return images;
+    }
 
-	/**
-     * Logs an exception to the Eclipse log file. 
+    /**
+     * Returns an image descriptor for the image file at the given
+     * plug-in relative path
+     *
+     * @param path the path
+     * @return the image descriptor
+     */
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
+
+    /**
+     * Logs an exception to the Eclipse log file.
      * 
      * This method detects the class and the method in which the exception was caught automatically using
      * the current stack trace. If required, the user can override these values by
@@ -149,10 +149,10 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
     }
 
     /**
-     * Logs an exception to the Eclipse log file. 
+     * Logs an exception to the Eclipse log file.
      * 
-     * Note that in most cases you can use the {@link #log(String, Exception)} method which automatically 
-     * detects the class and the method in which the exception was caught, so it requires as parameters 
+     * Note that in most cases you can use the {@link #log(String, Exception)} method which automatically
+     * detects the class and the method in which the exception was caught, so it requires as parameters
      * only a human-readable message and the exception.
      * 
      * @param className the name of the class in which the exception was caught.
@@ -177,21 +177,21 @@ public class Activator extends AbstractUIPlugin implements DebugOptionsListener 
         }
     }
 
-	/**
-	 * A UTC ISO 8601 date formatter used to log the time of errors.
-	 */
-	private static final DateFormat oDateFormatter;
-	static {
-		oDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
-		oDateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
-	}
+    /**
+     * A UTC ISO 8601 date formatter used to log the time of errors.
+     */
+    private static final DateFormat oDateFormatter;
+    static {
+        oDateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        oDateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
 
-	/**
-	 * Refreshes the debug trace sink when debug options have changed.
-	 */
-	@Override
-	public void optionsChanged(DebugOptions options) {
-		DEBUG = options.getBooleanOption(PLUGIN_ID + "/debug", false);
-		TRACE = options.newDebugTrace(PLUGIN_ID);
-	}
+    /**
+     * Refreshes the debug trace sink when debug options have changed.
+     */
+    @Override
+    public void optionsChanged(DebugOptions options) {
+        DEBUG = options.getBooleanOption(PLUGIN_ID + "/debug", false);
+        TRACE = options.newDebugTrace(PLUGIN_ID);
+    }
 }
