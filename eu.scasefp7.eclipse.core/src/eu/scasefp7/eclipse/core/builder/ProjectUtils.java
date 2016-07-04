1d2d3b0bd504cb2ae72940ffbac719ec6f83daf9
Marin Orlic <marin.orlic@ericsson.com>
2016-07-04 16:24:37 +0200

    Add utility method to add nature to project

--- eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/builder/ProjectUtils.java
+++ eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/builder/ProjectUtils.java
@@ -3,8 +3,13 @@ package eu.scasefp7.eclipse.core.builder;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
+
+import eu.scasefp7.eclipse.core.Activator;
 
 /**
  * Utility methods for project handling.
@@ -51,6 +56,27 @@ public class ProjectUtils
             project = (IProject) Platform.getAdapterManager().getAdapter(element, IProject.class);
         }
         return project;
+    }
+    
+    /**
+     * Adds the S-CASE nature to the project.
+     * 
+     * @param project to add the nature to
+     * @param monitor progress monitor
+     * @throws CoreException
+     */
+    public static void addNature(IProject project, IProgressMonitor monitor) throws CoreException {
+        try {
+            IProjectDescription description = project.getDescription();
+            String[] natures = description.getNatureIds();
+            String[] newNatures = new String[natures.length + 1];
+            System.arraycopy(natures, 0, newNatures, 0, natures.length);
+            newNatures[natures.length] = ScaseNature.NATURE_ID;
+            description.setNatureIds(newNatures);
+            project.setDescription(description, monitor);
+        } catch (CoreException e) {
+            Activator.log("Unable to add S-CASE project nature to project.", e);
+        }
     }
 }
 
