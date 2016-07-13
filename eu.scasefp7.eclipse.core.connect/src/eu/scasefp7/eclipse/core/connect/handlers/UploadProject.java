e6de8f720a2ab1a49894d2c297e4b93c4350a964
thdiaman <themisdiamantopoulos@hotmail.com>
2016-07-13 14:38:57 +0300

    Added handler for retrieving the project of a list of files

--- eu.scasefp7.eclipse.core.connect/src/eu/scasefp7/eclipse/core/connect/handlers/UploadProject.java
+++ eu.scasefp7.eclipse.core.connect/src/eu/scasefp7/eclipse/core/connect/handlers/UploadProject.java
@@ -2,18 +2,17 @@ package eu.scasefp7.eclipse.core.connect.handlers;
 
 import java.util.List;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import eu.scasefp7.eclipse.core.connect.uploader.ProjectAwareHandler;
 import eu.scasefp7.eclipse.core.connect.uploader.ProjectUploader;
 
-public class UploadProject extends AbstractHandler {
+public class UploadProject extends ProjectAwareHandler {
 
 	@Override
 	@SuppressWarnings("unchecked")
@@ -22,10 +21,8 @@ public class UploadProject extends AbstractHandler {
 		if (selection instanceof IStructuredSelection) {
 			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
 			List<Object> selectionList = structuredSelection.toList();
-			for (Object object : selectionList) {
-				IProject project = (IProject) Platform.getAdapterManager().getAdapter(object, IProject.class);
-				ProjectUploader.uploadProject(project);
-			}
+			IProject project = getProjectOfSelectionList(selectionList);
+			ProjectUploader.uploadProject(project);
 		}
 		return null;
 	}
