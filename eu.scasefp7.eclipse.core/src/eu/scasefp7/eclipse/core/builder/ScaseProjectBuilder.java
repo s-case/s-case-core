53b0511f9c0cc771cad1fd1f551d15856a9c899d
Marin Orlic <marin.orlic@ericsson.com>
2016-11-29 16:15:44 +0100

    Set marker idenfiers as public consts

--- eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/builder/ScaseProjectBuilder.java
+++ eu.scasefp7.eclipse.core/src/eu/scasefp7/eclipse/core/builder/ScaseProjectBuilder.java
@@ -40,10 +40,10 @@ public class ScaseProjectBuilder extends IncrementalProjectBuilder {
     public static final String BUILDER_ID = "eu.scasefp7.eclipse.core.scaseBuilder";
 
     /** Problem marker */
-    private static final String MARKER_TYPE = "eu.scasefp7.eclipse.core.problemMarker";
+    public static final String MARKER_TYPE = "eu.scasefp7.eclipse.core.problemMarker";
 
-    /** Configuration marker */
-    private static final String CONFIG_MARKER_TYPE = "eu.scasefp7.eclipse.core.configurationMarker";
+    /** Configuration problem marker */
+    public static final String CONFIG_MARKER_TYPE = "eu.scasefp7.eclipse.core.configurationMarker";
     
     class SampleDeltaVisitor implements IResourceDeltaVisitor {
 
